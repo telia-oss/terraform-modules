d3d8eee19a04334a1e7bf1bcdf23b47d4d9d729f
glennbech <glenn.bech@gmail.com>
2019-02-25 13:48:34 +0100

    removed sec group id

--- elasticcache/example/default/main.tf
+++ elasticcache/example/default/main.tf
@@ -6,16 +6,9 @@ data "aws_subnet_ids" "main" {
   vpc_id = "${data.aws_vpc.main.id}"
 }
 
-resource "aws_security_group" "cache_sec_group" {
-  name        = "exammple_sg"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = "${data.aws_vpc.main.id}"
-}
-
 module "example_elasticcache_cluster" {
-  source                   = "../.."
-  prefix                   = "example"
-  vpc_id                   = "${data.aws_vpc.main.id}"
-  subnet_ids               = "${data.aws_subnet_ids.main.ids}"
-  source_security_group_id = "${aws_security_group.cache_sec_group.id}"
+  source     = "../.."
+  prefix     = "example"
+  vpc_id     = "${data.aws_vpc.main.id}"
+  subnet_ids = "${data.aws_subnet_ids.main.ids}"
 }
